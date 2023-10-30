292fdfb56475a59f253fbf2e54b947dab7636525
Mohamed Mahmoud <m.raslan97@gmail.com>
2023-10-30 19:14:27 +0200

    sign in with Google -> done

---
+++ lib/testing_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'Firebase_services/sign_in_with_google_service.dart';
+
+class TestingScreen extends StatefulWidget {
+  const TestingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TestingScreen> createState() => _TestingScreenState();
+}
+
+class _TestingScreenState extends State<TestingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children:[
+            ElevatedButton(onPressed: (){
+              signInWithGoogle();
+            },
+                child: Text("Sign in with Google")),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
